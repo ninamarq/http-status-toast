f5d18d77a9b58ebc24d2974b65353982b724f8e5
Marina Marques <marina.marqueso@outlook.com>
2023-07-05 23:18:26 -0300

    feat: publishing config

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,11 @@
-export * from './components';
+import React from 'react';
+import { IToastProps, Toast } from './components/Toast';
+import * as components from './components';
+
+export { components };
+
+const httpStatusToast = (props: IToastProps) => {
+	return <Toast {...props} />;
+};
+
+export default httpStatusToast;
